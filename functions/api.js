e402c5b8e22c342a2e23aefae8e6eae63b7c534a
codingANDdecodin <130179661+codingANDdecodin@users.noreply.github.com>
2023-04-27 17:39:44 +0530

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -4,9 +4,12 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const cors=require("cors");
 
 const router = express.Router();
+app.use(cors())
 router.get('/', (req, res) => {
+  res.setHeader("Access-Control-Allow-Credential","true")
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
@@ -15,6 +18,7 @@ router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
+
 app.use('/.netlify/functions/api', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')));
 
